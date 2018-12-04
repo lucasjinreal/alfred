e351d965df228cb132c3320a6ff9e0f5ca948488
Your Name <you@example.com>
2018-12-04 11:49:53 +0800

    add

--- alfred/vis/image/seg.py
+++ alfred/vis/image/seg.py
@@ -10,7 +10,7 @@ from .get_dataset_colormap import label_to_color_image
 from .get_dataset_colormap import _ADE20K, _CITYSCAPES, _MAPILLARY_VISTAS, _PASCAL
 
 
-def draw_seg_by_dataset(img, seg, dataset, alpha=0.5, is_show=False, bgr_in=False):
+def draw_seg_by_dataset(img, seg, dataset, alpha=0.7, is_show=False, bgr_in=False):
     assert dataset in [_PASCAL, _CITYSCAPES, _MAPILLARY_VISTAS, _ADE20K], 'dataset not support yet.'
     img = np.asarray(img, dtype=np.uint8)
     if bgr_in:
@@ -23,7 +23,7 @@ def draw_seg_by_dataset(img, seg, dataset, alpha=0.5, is_show=False, bgr_in=Fals
         # resize mask to img shape
         mask_color = cv2.resize(mask_color, (img.shape[1], img.shape[0]))
 
-    res = cv2.addWeighted(img, 0.3, mask_color, 0.7, 0.6)
+    res = cv2.addWeighted(img, 0.3, mask_color, alpha, 0.6)
     if is_show:
         cv2.imshow('result', res)
         cv2.waitKey(0)
