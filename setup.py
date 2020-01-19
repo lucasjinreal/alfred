e6bec0d20724add305e48813a71a38a6ee006386
FagangJin <fagangjin@autox.ai>
2020-01-19 17:42:26 +0800

    add

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, Extension
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'readme.md')) as f:
     long_description = f.read()
 
 
