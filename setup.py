a94f7ed52e35bd2bcd654c1cfd9a3127e394d249
Your Name <you@example.com>
2020-02-01 15:13:55 +0800

    add

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from os import path
 
 
 this_directory = path.abspath(path.dirname(__file__))
-with io.open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
