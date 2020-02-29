fabd03a98cc631691ae4cf81bc65536f1ab5c905
Your Name <you@example.com>
2020-02-29 11:56:59 +0800

    batched upgrade license of alfred

--- alfred/modules/scrap/__init__.py
+++ alfred/modules/scrap/__init__.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
-# file: __init__.py
-# author: JinTian
-# time: 05/02/2018 9:23 PM
-# Copyright 2018 JinTian. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Copyright (c) 2020 JinTian.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# This file is part of alfred
+# (see http://jinfagang.github.io).
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ------------------------------------------------------------------------
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
