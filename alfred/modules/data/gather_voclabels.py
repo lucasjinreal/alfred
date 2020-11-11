b2ad9ce02482c9ea7734333e320b291b3ab26563
FagangJin <fagangjin@autox.ai>
2020-11-11 15:21:35 +0800

    add voc data statistics

--- alfred/modules/data/gather_voclabels.py
+++ alfred/modules/data/gather_voclabels.py
@@ -47,15 +47,35 @@ import glob
 def gather_labels(anno_dir):
     all_labels = glob.glob(os.path.join(anno_dir, '*.xml'))
     all_names = []
+    all_obj_num = 0
+    xmls_without_boxes = []
+    i = 0
+    cls_num_map = dict()
     for label in all_labels:
-        print('parsing {}'.format(label))
+        if i % 500 == 0:
+            print('parsing [{}/{}] {}'.format(i, len(all_labels), label))
+        i += 1
         root = ET.parse(label).getroot()
+        one_sample_obj_num = 0
         for obj in root.iter('object'):
+            one_sample_obj_num += 1
             name = obj.find('name').text
+            if name in cls_num_map.keys():
+                cls_num_map[name] += 1
+            else:
+                cls_num_map[name] = 0
             if name not in all_names:
                 all_names.append(name)
+        if one_sample_obj_num == 0:
+            xmls_without_boxes.append(label)
+        all_obj_num += one_sample_obj_num
     print('Done. summary...')
     print('all {} classes.'.format(len(all_names)))
     print(all_names)
+    # we also read xmls with empty boxes
+    print('\nclass boxes statistic as: {}'.format(cls_num_map))
+    if len(xmls_without_boxes) > 0:
+        print('\nalso, we found these files without any detections, you can consider remove it:')
+        print(xmls_without_boxes)
 
 
