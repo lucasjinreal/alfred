2759c9733fbb49b7edd9c3036fa72b056d9db457
Your Name <you@example.com>
2021-12-23 17:28:40 +0800

    add

--- alfred/deploy/tensorrt/common.py
+++ alfred/deploy/tensorrt/common.py
@@ -139,6 +139,33 @@ def allocate_buffers_v2_dynamic(engine, is_explicit_batch=False, input_shape=Non
     return inputs, outputs, bindings, stream
 
 
+def allocate_buffers_v2_dynamic_batch(engine, is_explicit_batch=False, max_batch=1):
+    inputs = []
+    outputs = []
+    bindings = []
+    stream = cuda.Stream()
+    for binding in engine:
+        dims = engine.get_binding_shape(binding)
+        if dims[-1] == -1:
+            assert (
+                input_shape is not None
+            ), "dynamic trt engine must specific input_shape"
+            dims[-2], dims[-1] = input_shape
+        size = trt.volume(dims) * engine.max_batch_size
+        dtype = trt.nptype(engine.get_binding_dtype(binding))
+        # Allocate host and device buffers
+        host_mem = cuda.pagelocked_empty(size, dtype)
+        device_mem = cuda.mem_alloc(host_mem.nbytes)
+        # Append the device buffer to device bindings.
+        bindings.append(int(device_mem))
+        # Append to the appropriate list.
+        if engine.binding_is_input(binding):
+            inputs.append(HostDeviceMem(host_mem, device_mem))
+        else:
+            outputs.append(HostDeviceMem(host_mem, device_mem))
+    return inputs, outputs, bindings, stream
+
+
 def do_inference_v2_dynamic(context, bindings, inputs, outputs, stream, input_tensor):
     """
     this works for infer on dynamic engine, such as input dynamic
