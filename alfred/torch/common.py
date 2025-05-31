1f2551b0622aedefaa76a594c412a386056d50f6
Your Name <you@example.com>
2025-05-31 21:45:22 +0800

    add

---
+++ alfred/torch/common.py
@@ -0,0 +1,3 @@
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- alfred/version.py
+++ alfred/version.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 major_num = 2
 
-__version__ = "3.0.14"
+__version__ = "3.0.15"
 short_version = __version__
 
 
